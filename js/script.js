036d4a587ad01dc5d0475de9145d35d2c3a99143
MarinaSDIniz <marinasilvadiniz@gmail.com>
2024-12-27 17:26:32 -0300

    Update
    mudanças no css, html, js e img

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-times'); // Altera o ícone entre "hambúrguer" e "x"
     navbar.classList.toggle('active'); // Mostra ou esconde o menu
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const offset = document.querySelector('header').offsetHeight;
+      const topPosition = target.offsetTop - offset;
+  
+      window.scrollTo({
+        top: topPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
